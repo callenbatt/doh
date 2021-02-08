f4d49c009f68dcdb35b75a99d291f81b90f0fba9
Chris Batt <chris.batt@finalsite.com>
2021-02-07 21:50:31 -0500

    added comments to cloudflare controller

--- functions/controllers/cloudflare.js
+++ functions/controllers/cloudflare.js
@@ -1,5 +1,10 @@
 const fetch = require("node-fetch");
 
+/**
+ * Request data from the cloudflare hostnames endpoint
+ * @param {Number} page
+ * @returns {JSON} response data
+ */
 const requestHostnames = async (page = 1) => {
   try {
     const response = await fetch(
@@ -13,7 +18,13 @@ const requestHostnames = async (page = 1) => {
   }
 };
 
-const formatResult = (result) => {
+/**
+ * Formats the hostname API response to just the
+ * relevant data
+ * @param {Array} result of hostname fetch
+ * @returns {Array} of formatted hostname data
+ */
+const formatHostnameResult = (result) => {
   return result.map((item) => {
     return {
       hostname: item.hostname,
@@ -25,18 +36,44 @@ const formatResult = (result) => {
   });
 };
 
+/**
+ * Creates an array of numbers for fetching multiple
+ * pages in an API
+ * @param {Number} pages final number in array
+ * @param {Number} offset page to start the array
+ * @returns {Array} of page numbers
+ */
+const createArrayOfPages = (pages, offset = 1) => {
+  return Array.from(Array(pages - offset), (e, i) => i + (1 + offset));
+};
+
+/**
+ * Formats an array of hostname data, organized by tenant_name
+ * @param {Array} hostnames returned from getAllHostnames
+ * @returns {Array} organized by tenant_name 
+ */
+const formatAllHostnames = (hostnames) => {
+  return hostnames.reduce((a, c) => {
+    a.hostname ? a.hostname.push(c) : (a[c.hostname] = c);
+    return a;
+  }, {});
+};
+
+/**
+ * Fetches all pages of hostname data from Cloudflare API
+ * @returns {Array} of formatted hostname data
+ */
 const getAllHostnames = async () => {
   const firstPage = await requestHostnames();
-  const pages = Array.from(
-    Array(firstPage.result_info.total_pages - 1),
-    (e, i) => i + 2
-  );
-  const firstData = formatResult(firstPage.result);
+  const firstData = formatHostnameResult(firstPage.result);
+  const restPages = createArrayOfPages(firstPage.result_info.total_pages);
   const restData = await Promise.all(
-    pages.map(async (page) => {
+    restPages.map(async (page) => {
       const response = await requestHostnames(page);
-      return formatResult(response.result);
+      return formatHostnameResult(response.result);
     })
   );
-  return [...firstData, ...restData.flat()]
+  const hostnamesNew = formatAllHostnames([...firstData, ...restData.flat()]);
 };
+
+
